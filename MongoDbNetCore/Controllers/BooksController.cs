be444194633c38969fc02b6de8212c4c8be7ea67
Do Thang <dothangbk1998@gmail.com>
2020-01-13 16:58:24 +0700

    simple crud

---
+++ MongoDbNetCore/Controllers/BooksController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MongoDbNetCore.Models;
+using MongoDbNetCore.Services;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MongoDbNetCore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : Controller
+    {
+        private readonly BookService _bookService;
+
+        public BooksController(BookService bookService)
+        {
+            _bookService = bookService;
+        }
+        // GET: api/values
+        [HttpGet]
+        public ActionResult<List<Book>> Get()
+        {
+            return _bookService.Get();
+        }
+
+        // GET api/values/5
+        [HttpGet("{id:length(24)}", Name = "GetBook")]
+        public ActionResult<Book> Get(string id)
+        {
+            var book = _bookService.Get(id);
+
+            if (book == null)
+            {
+                return NotFound();
+            }
+            return book;
+        }
+
+        // POST api/values
+        [HttpPost]
+        public ActionResult<Book> Create(Book book)
+        {
+            _bookService.Create(book);
+            return CreatedAtRoute("GetBook", new { id = book.Id.ToString() }, book);
+        }
+
+        [HttpPut("{id:length(24)}")]
+        public IActionResult Update(string id, Book bookIn)
+        {
+            var book = _bookService.Get(id);
+
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            _bookService.Update(id, bookIn);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id:length(24)}")]
+        public IActionResult Delete(string id)
+        {
+            var book = _bookService.Get(id);
+
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            _bookService.Remove(book.Id);
+
+            return NoContent();
+        }
+    }
+}
---
+++ MongoDbNetCore/Services/BookService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using MongoDB.Driver;
+using MongoDbNetCore.Models;
+
+namespace MongoDbNetCore.Services
+{
+    public class BookService
+    {
+        private readonly IMongoCollection<Book> _books;
+        public BookService(IBookstoreDatabaseSettings settings)
+        {
+            var client = new MongoClient(settings.ConnectionString);
+            var database = client.GetDatabase(settings.DatabaseName);
+
+            _books = database.GetCollection<Book>(settings.BooksCollectionName);
+        }
+
+        public List<Book> Get() => this._books.Find(book => true).ToList();
+
+        public Book Get(string id) => this._books.Find<Book>(book => book.Id == id).FirstOrDefault();
+
+        public Book Create(Book book)
+        {
+            this._books.InsertOne(book);
+            return book;
+
+        }
+
+        public void Update(string id, Book bookIn) => this._books.ReplaceOne(book => book.Id == id, bookIn);
+
+        public void Remove(Book bookIn) => this._books.DeleteOne(book => book.Id == bookIn.Id);
+
+        public void Remove(string id) => this._books.DeleteOne(book => book.Id == id);
+    }
+}
